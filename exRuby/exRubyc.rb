7e0780dd873cb24554084c65deacf87e32a1c053
Matheus Negrão <matheus.dnegrao@gmail.com>
2019-04-15 22:14:17 -0300

    Exemplo Transação Correta Ruby Postgres

---
+++ exRuby/exRubyc.rb
@@ -0,0 +1,26 @@
+require 'pg'
+
+begin
+
+    con = PG.connect :dbname => 'aulas', :user => 'matheus'
+    
+    con.transaction do |con|
+        
+        con.exec "UPDATE Cars SET Price=23700 WHERE Id=8"
+        con.exec "INSERT INTO Cars VALUES(9,'Mazda',27770)"
+        con.exec "ROLLBACK"
+    
+    end
+    
+rescue PG::Error => e
+
+	puts 'ROLLBACK'
+    puts e.message
+
+else
+	puts 'COMMIT' 
+    
+ensure
+
+    con.close if con   
+end
